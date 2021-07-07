94ff1ca14cb2576de38353f8709842aa9306b039
Laura <lnkeat@gmail.com>
2021-07-07 10:58:11 -0600

    commit tree 0d69b07a2842be29fe5ddc1f9688c8ff27ade2b0

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(rideLength){
   // Write your code here!
+  if(rideLength <= 400){
+    return 'This one is on me!'
+  } else if(rideLength > 2000 && rideLength < 2500){
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC' ? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(amount){
   // Write your code here!
+  switch(amount){
+    case 'generous' :
+      return "Thank you so much.";
+      break;
+    case 'not as generous' :
+      return 'Thank you.';
+      break;
+    default :
+      return 'Bye.'
+  }
 }
